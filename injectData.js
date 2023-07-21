0fff3fde388a842e3ae917c642e149507f4494b9
Moises R Sanchez <59586957+MoisesSanchez2020@users.noreply.github.com>
2023-07-20 19:43:57 -0700

    Add files via upload

--- injectData.js
+++ injectData.js
@@ -1,5 +1,5 @@
 // injectData.js
-
+/*
 function injectDataIntoTemplate(data) {
     const fullNameElement = document.getElementById("svgfullname");
     const idNumberElement = document.getElementById("svgidnumber");
@@ -19,6 +19,42 @@ function injectDataIntoTemplate(data) {
         width: 180,
         height: 180,
     });
+}
+
+// Fetch the data from the data.json file
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // Assuming there is only one result in the data.json file
+        const personData = data.results[0];
+        injectDataIntoTemplate(personData);
+    })
+    .catch((error) => {
+        console.error("Error fetching or parsing JSON:", error);
+    });*/
+
+
+    // injectData.js
+async function injectDataIntoTemplate(data) {
+    const fullNameElement = document.getElementById("svgfullname");
+    const idNumberElement = document.getElementById("svgidnumber");
+    const dobElement = document.getElementById("svgdob");
+    const pictureElement = document.getElementById("picture");
+    const qrCodeElement = document.getElementById("qrcode");
+
+    fullNameElement.textContent = data.name + " " + data.last_name;
+    idNumberElement.textContent = data.driver_license;
+    dobElement.textContent = data.date_of_birth;
+
+    pictureElement.setAttribute("xlink:href", data.avatar);
+
+    // Create the QR code and add it to the qrcode element
+    const writer = new ZXing.BarcodeFormat.QR_CODE.Writer();
+    let matrix = writer.encode(data.id, ZXing.BarcodeFormat.QR_CODE, 180, 180);
+    let svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    let svgMatrix = new ZXing.BrowserSvgCodeWriter().createDomMatrix(matrix)
+    svgElement.appendChild(svgMatrix);
+    qrCodeElement.appendChild(svgElement);
 }
 
 // Fetch the data from the data.json file
