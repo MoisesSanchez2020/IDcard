f0c40f2a0cad5e8c22139bd340d1e4e55409babc
Moises R Sanchez <59586957+MoisesSanchez2020@users.noreply.github.com>
2023-07-20 18:54:06 -0700

    Add files via upload

---
+++ injectData.js
@@ -0,0 +1,34 @@
+// injectData.js
+
+function injectDataIntoTemplate(data) {
+    const fullNameElement = document.getElementById("svgfullname");
+    const idNumberElement = document.getElementById("svgidnumber");
+    const dobElement = document.getElementById("svgdob");
+    const pictureElement = document.getElementById("picture");
+    const qrCodeElement = document.getElementById("qrcode");
+
+    fullNameElement.textContent = data.name + " " + data.last_name;
+    idNumberElement.textContent = data.driver_license;
+    dobElement.textContent = data.date_of_birth;
+
+    pictureElement.setAttribute("xlink:href", data.avatar);
+
+    // Create the QR code and add it to the qrcode element
+    const qrCode = new QRCode(qrCodeElement, {
+        text: data.id,
+        width: 180,
+        height: 180,
+    });
+}
+
+// Fetch the data from the data.json file
+fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        // Assuming there is only one result in the data.json file
+        const personData = data.results[0];
+        injectDataIntoTemplate(personData);
+    })
+    .catch((error) => {
+        console.error("Error fetching or parsing JSON:", error);
+    });
